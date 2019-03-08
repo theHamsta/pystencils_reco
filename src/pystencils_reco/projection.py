5971f93064e65afcccf0b38e888cf1ea2296a97b
Stephan Seitz <stephan.seitz@fau.de>
2019-03-08 19:25:06 +0100

    Add stub for projection.py

---
+++ src/pystencils_reco/projection.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2019 seitz_local <seitz_local@lmeXX>
+#
+# Distributed under terms of the GPLv3 license.
+
+"""
+Implements a generic forward and backprojection projections
+"""
+
+
