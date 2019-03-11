bd513b2a24a43ccb2efef411d9482ea714b1fdc3
Stephan Seitz <stephan.seitz@fau.de>
2019-03-11 12:52:58 +0100

    Add test_stencils.test_BallStencil

--- tests/test_stencils.py
+++ tests/test_stencils.py
@@ -16,6 +16,14 @@ def test_BoxStencil():
                 print(stencil)
 
 
+def test_BallStencil():
+    for with_center in (True, False):
+        for ndim in range(1, 4):
+            for radius in range(1, 5):
+                stencil = BallStencil(radius, ndim, with_center=with_center)
+                print(stencil)
+
+
 def test_BoxStencil_non_quadratic():
     stencil = BoxStencil((3, 5))
     print(stencil)
@@ -23,6 +31,7 @@ def test_BoxStencil_non_quadratic():
 
 def main():
     test_BoxStencil()
+    test_BallStencil()
     test_BoxStencil_non_quadratic()
 
 
