8d04dd9e3b4aa5c004ffebd051f19385786d13cf
Stephan Seitz <stephan.seitz@fau.de>
2019-03-11 16:33:13 +0100

    Refactor: move handling of with_center to base class pystencils_reco.stencils.Stencil

--- src/pystencils_reco/stencils.py
+++ src/pystencils_reco/stencils.py
@@ -20,10 +20,13 @@ from pystencils.stencils import *  # NOQA
 class Stencil(list):
     """Implements an Iterable of relative accesses on a Field"""
 
-    def __init__(self, iterable, ndim):
+    def __init__(self, iterable, ndim, with_center=True):
         assert (len(i) == ndim for i in iterable), "All elements of a Stencils should have len == ndim"
         super(Stencil, self).__init__(list(iterable))
         self.ndim = ndim
+
+        if not with_center:
+            self.remove(tuple([0]*ndim))
 
 
 class LineStencil(Stencil):
@@ -33,9 +36,7 @@ class LineStencil(Stencil):
         stencil = [0] * ndim
         stencil[filter_dimension] = range(-(kernel_size//2), kernel_size//2 + 1)
 
-        if not with_center:
-            stencil.remove(tuple([0]*ndim))
-        super(LineStencil, self).__init__(stencil, ndim)
+        super(LineStencil, self).__init__(stencil, ndim, with_center)
 
 
 class BoxStencil(Stencil):
@@ -48,9 +49,6 @@ class BoxStencil(Stencil):
                               pampy.ANY, lambda _: itertools.product(
                                   *[range(-(i//2), i//2 + 1) for i in kernel_size])
                               )
-        if not with_center:
-            stencil = list(stencil)
-            stencil.remove(tuple([0]*ndim))
 
         if isinstance(kernel_size, int):
             kernel_size = [kernel_size] * ndim
@@ -58,7 +56,7 @@ class BoxStencil(Stencil):
             ndim = len(kernel_size)
         assert (i % 2 == 1 for i in kernel_size), "kernel_size must consist of uneven numbers"
 
-        super(BoxStencil, self).__init__(stencil, ndim)
+        super(BoxStencil, self).__init__(stencil, ndim, with_center)
 
 
 class BallStencil(Stencil):
@@ -77,7 +75,4 @@ class BallStencil(Stencil):
             if norm <= radius:
                 stencil.append(s)
 
-        if not with_center:
-            stencil.remove(tuple([0]*ndim))
-
-        super(BallStencil, self).__init__(stencil, ndim)
+        super(BallStencil, self).__init__(stencil, ndim, with_center)
