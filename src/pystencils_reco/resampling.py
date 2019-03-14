25825cec1e242a4834504d9c8f5c7c43c658ad85
Stephan Seitz <stephan.seitz@fau.de>
2019-03-14 15:44:23 +0100

    Formating to silence warnings in pystencils_reco.resampling

--- src/pystencils_reco/resampling.py
+++ src/pystencils_reco/resampling.py
@@ -23,8 +23,9 @@ def generic_spatial_matrix_transform(input_field, output_field, transform_matrix
         inverse_matrix = transform_matrix.inv()
 
     assignments = AssignmentCollection({
-        output_field.center(): texture.at(input_field.coordinate_transform.inv() @
-                                          (inverse_matrix @ output_field.physical_coordinates_staggered) - input_field.coordinate_origin)
+        output_field.center():
+        texture.at(input_field.coordinate_transform.inv() @
+                   (inverse_matrix @ output_field.physical_coordinates_staggered) - input_field.coordinate_origin)
     })
     assignments.transform_matrix = transform_matrix
 
@@ -57,7 +58,11 @@ def rotation_transform(input_field, output_field, rotation_angle, rotation_axis=
     else:
         raise NotImplementedError('Rotations only implemented for 2d and 3d')
 
-    return generic_spatial_matrix_transform(input_field, output_field, transform_matrix, inverse_matrix=transform_matrix.T)
+    return generic_spatial_matrix_transform(input_field,
+                                            output_field,
+                                            transform_matrix,
+                                            inverse_matrix=transform_matrix.T)
+
 
 def resample(input_field, output_field):
     """
@@ -67,5 +72,6 @@ def resample(input_field, output_field):
     :param output_field:
     """
 
-    return generic_spatial_matrix_transform(input_field, output_field, sympy.Matrix(sympy.Identity(input_field.spatial_dimensions)))
-
+    return generic_spatial_matrix_transform(input_field,
+ output_field,
+ sympy.Matrix(sympy.Identity(input_field.spatial_dimensions)))
