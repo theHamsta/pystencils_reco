1ef13e795132a983f3f18b43eadb38e880f96cff
Stephan Seitz <stephan.seitz@fau.de>
2019-03-14 15:42:51 +0100

    Optimize resampling.rotation_transform/ add resampling.resample

--- src/pystencils_reco/resampling.py
+++ src/pystencils_reco/resampling.py
@@ -16,12 +16,15 @@ import pystencils
 from pystencils_reco import AssignmentCollection
 
 
-def generic_spatial_matrix_transform(input_field, output_field, transform_matrix):
+def generic_spatial_matrix_transform(input_field, output_field, transform_matrix, inverse_matrix=None):
     texture = pystencils.astnodes.TextureCachedField(input_field)
-    ndim = input_field.spatial_dimensions
+
+    if inverse_matrix is None:
+        inverse_matrix = transform_matrix.inv()
 
     assignments = AssignmentCollection({
-        output_field.center(): texture.at(transform_matrix.inv() @ pystencils.x_staggered_vector(ndim))
+        output_field.center(): texture.at(input_field.coordinate_transform.inv() @
+                                          (inverse_matrix @ output_field.physical_coordinates_staggered) - input_field.coordinate_origin)
     })
     assignments.transform_matrix = transform_matrix
 
@@ -49,8 +52,20 @@ def rotation_transform(input_field, output_field, rotation_angle, rotation_axis=
         assert rotation_axis is not None, "You must specify a rotation_axis for 3d rotations!"
         transform_matrix = getattr(sympy, 'rot_axis%i' % (rotation_axis+1))(rotation_angle)
     elif input_field.spatial_dimensions == 2:
-        transform_matrix = sympy.rot_axis1(rotation_angle)[:2, :2]
+        # 2d rotation is 3d rotation around 3rd axis
+        transform_matrix = sympy.rot_axis3(rotation_angle)[:2, :2]
     else:
         raise NotImplementedError('Rotations only implemented for 2d and 3d')
 
-    return generic_spatial_matrix_transform(input_field, output_field, transform_matrix)
+    return generic_spatial_matrix_transform(input_field, output_field, transform_matrix, inverse_matrix=transform_matrix.T)
+
+def resample(input_field, output_field):
+    """
+    Resample input_field with its coordinate system in terms of the coordinate system of output_field
+
+    :param input_field:
+    :param output_field:
+    """
+
+    return generic_spatial_matrix_transform(input_field, output_field, sympy.Matrix(sympy.Identity(input_field.spatial_dimensions)))
+
--- tests/test_resampling.py
+++ tests/test_resampling.py
@@ -9,8 +9,8 @@
 """
 import os
 from os.path import dirname, join
-import numpy as np
 
+import numpy as np
 import pytest
 import skimage.io
 import sympy
@@ -69,7 +69,50 @@ def test_scaling_visualize():
     test_image = to_gpu(test_image)
     tmp = zeros_like(test_image)
 
-    for s in (0.2, 0.5, 0.7, 2):
+    for s in (0.2, 0.5, 0.7, 1, 2):
+        transform(x=test_image, y=tmp, s=s)
+        pyconrad.imshow(tmp.get(), str(s))
+
+
+@pytest.mark.skipif("CI" in os.environ and os.environ["CI"] == "true", reason="Skip GUI tests on CI")
+def test_rotation_visualize():
+    import pyconrad.autoinit
+    from pycuda.gpuarray import to_gpu, zeros_like
+
+    x, y = pystencils.fields('x,y: float32[2d]')
+    s = pystencils.data_types.TypedSymbol('s', 'float32')
+    transform = rotation_transform(x, y, s).compile('gpu')
+
+    test_image = 1-skimage.io.imread(join(dirname(__file__), "test_data",  "test_vessel2d_mask.png"), as_gray=True)
+    test_image = np.ascontiguousarray(test_image, np.float32)
+    test_image = to_gpu(test_image)
+    tmp = zeros_like(test_image)
+    print(transform.code)
+
+    for s in (0.2, 0.5, 0.7, 1, 2):
+        transform(x=test_image, y=tmp, s=s)
+        pyconrad.imshow(tmp.get(), str(s))
+
+
+@pytest.mark.skipif("CI" in os.environ and os.environ["CI"] == "true", reason="Skip GUI tests on CI")
+def test_rotation_around_center_visualize():
+    import pyconrad.autoinit
+    from pycuda.gpuarray import to_gpu, zeros_like
+
+    test_image = 1-skimage.io.imread(join(dirname(__file__), "test_data",  "test_vessel2d_mask.png"), as_gray=True)
+    test_image = np.ascontiguousarray(test_image, np.float32)
+    test_image = to_gpu(test_image)
+
+    tmp = zeros_like(test_image)
+    x, y = pystencils.fields(f'x,y: float32[{",".join(str(i) for i in test_image.shape)}]')
+    x.set_coordinate_origin_to_field_center()
+    y.set_coordinate_origin_to_field_center()
+    print(x.coordinate_origin)
+    s = pystencils.data_types.TypedSymbol('s', 'float32')
+    transform = rotation_transform(x, y, s).compile('gpu')
+    print(transform.code)
+
+    for s in (0, 0.2, 0.5, 0.7, 1, 2):
         transform(x=test_image, y=tmp, s=s)
         pyconrad.imshow(tmp.get(), str(s))
 
@@ -79,7 +122,9 @@ def main():
     # test_rotation()
     # test_scaling_compilation()
     # test_rotation_compilation()
-    test_scaling_visualize()
+    # test_scaling_visualize()
+    # test_rotation_visualize()
+    test_rotation_around_center_visualize()
 
 
 if __name__ == '__main__':
