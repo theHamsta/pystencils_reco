ad2c7e458dd8ccba87a7686151234356bb3f7184
Stephan Seitz <stephan.seitz@fau.de>
2019-03-16 18:37:47 +0100

    Add ray equation test to test.test_projective_matrix

--- tests/test_projective_matrix.py
+++ tests/test_projective_matrix.py
@@ -22,6 +22,27 @@ def test_projective_matrix():
     result = matrix @ vector
     print(result)
 
+    print('Perspective projection')
+    projection_matrix = ProjectiveMatrix([[-289.0098977737411, -1205.2274801832275, 0.0, 186000.0],
+                                          [-239.9634468375339, - 4.188577544948043, 1200.0, 144000.0],
+                                          [-0.9998476951563913, -0.01745240643728351, 0.0, 600.0]])
+    x = sympy.Matrix(sympy.symbols('x:3'))
+    u = sympy.Matrix(sympy.symbols('u:2'))
+    eqn = projection_matrix @ x - u
+    ray_equations = sympy.solve(eqn, x)
+    for k, v in ray_equations.items():
+        print(f"{k}: {v}")
+
+    print('Orthogonal projection')
+    projection_matrix = ProjectiveMatrix([[1, 0, 0],
+                                          [0, 1, 0]])
+    x = sympy.Matrix(sympy.symbols('x:3'))
+    u = sympy.Matrix(sympy.symbols('u:2'))
+    eqn = projection_matrix @ x - u
+    ray_equations = sympy.solve(eqn, x)
+    for k, v in ray_equations.items():
+        print(f"{k}: {v}")
+
 
 def main():
     test_projective_matrix()
