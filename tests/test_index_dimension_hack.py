3c1833d68f8dc2fe3ab0209a2cec86dc139129c3
Stephan Seitz <stephan.seitz@fau.de>
2019-10-25 19:05:20 +0200

    Add test_index_dimension_hack

---
+++ tests/test_index_dimension_hack.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2019 Stephan Seitz <stephan.seitz@fau.de>
+#
+# Distributed under terms of the GPLv3 license.
+
+"""
+
+"""
+
+from pystencils.field import FieldType
+from pystencils_autodiff.field_tensor_conversion import \
+    ArrayWithIndexDimensions
+from pystencils_reco import crazy
+
+
+@crazy
+def foo(x, y):
+    print(x)
+    print(y)
+    print("y.index_dimensions: " + str(y.index_dimensions))
+
+
+def test_index_dimension_hack():
+    import numpy as np
+
+    x = np.zeros((20, 10))
+    y = ArrayWithIndexDimensions(np.zeros((20, 10)), 1, FieldType.CUSTOM)
+    print(y.shape)
+
+    foo(x, y)
