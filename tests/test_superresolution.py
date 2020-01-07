312db1af24d9ca0da2f0723c366e06e1d03f75fa
Stephan Seitz <stephan.seitz@fau.de>
2020-01-07 11:57:19 +0100

    Add test_polar_transform

--- tests/test_superresolution.py
+++ tests/test_superresolution.py
@@ -11,9 +11,10 @@ from os.path import dirname, join
 
 import numpy as np
 import skimage.io
+import sympy
 
 import pystencils
-from pystencils_reco.resampling import downsample, scale_transform, translate, upsample
+from pystencils_reco.resampling import downsample, resample, scale_transform, translate
 
 try:
     import pyconrad.autoinit
@@ -62,3 +63,20 @@ def test_warp():
 
     for i in range(len(warped)):
         warp_kernel(lenna[i], warped[i], warp_vectors[i])
+
+
+def test_polar_transform():
+    x, y = pystencils.fields('x, y:  float32[2d]')
+
+    x.set_coordinate_origin_to_field_center()
+    y.set_coordinate_origin_to_field_center()
+    y.coordinate_transform = lambda x: sympy.Matrix((x.norm(), sympy.atan2(*x) / (2 * sympy.pi) * y.shape[1]))
+
+    lenna_file = join(dirname(__file__), "test_data", "lenna.png")
+    lenna = skimage.io.imread(lenna_file, as_gray=True).astype(np.float32)
+
+    transformed = np.zeros((500, 500), np.float32)
+
+    resample(x, y).compile()(x=lenna, y=transformed)
+
+    pyconrad.imshow(transformed)
